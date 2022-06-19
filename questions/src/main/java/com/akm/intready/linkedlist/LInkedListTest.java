c207a9bb538e067b9acd819e7d317c886e3ffa3a
akm7-git <alok.mishra.git@gmail.com>
2022-06-19 11:18:56 +0530

    Committing linked list delete

--- questions/src/main/java/com/akm/intready/linkedlist/LInkedListTest.java
+++ questions/src/main/java/com/akm/intready/linkedlist/LInkedListTest.java
@@ -5,12 +5,12 @@ public class LInkedListTest {
     public static void main(String[] args) {
        Node head =  LinkedListUtils.createLinkedList(2,3,4,5,6,7);
         LinkedListUtils.printLinkedList(head);
-        System.out.println("Length of the linkedlist is : " + LinkedListUtils.getLengthOfLinkedList(head));
+        System.out.println("Length of the linked list is : " + LinkedListUtils.getLengthOfLinkedList(head));
         head = LinkedListUtils.insertNode(head, 10, 4);
-        System.out.println("Length of the linkedlist is : " + LinkedListUtils.getLengthOfLinkedList(head));
+        System.out.println("Length of the linked list is : " + LinkedListUtils.getLengthOfLinkedList(head));
         LinkedListUtils.printLinkedList(head);
         LinkedListUtils.deleteNode(head,4);
-        System.out.println("Length of the linkedlist is : " + LinkedListUtils.getLengthOfLinkedList(head));
+        System.out.println("Length of the linked list is : " + LinkedListUtils.getLengthOfLinkedList(head));
         LinkedListUtils.printLinkedList(head);
     }
 }
