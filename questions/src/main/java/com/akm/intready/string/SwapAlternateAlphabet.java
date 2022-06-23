701920862a74ee1e219b0a5d63ce7a03123423a5
akm7-git <alok.mishra.git@gmail.com>
2022-06-23 21:23:03 +0530

    String question

---
+++ questions/src/main/java/com/akm/intready/string/SwapAlternateAlphabet.java
@@ -0,0 +1,31 @@
+package com.akm.intready.string;
+
+public class SwapAlternateAlphabet {
+
+    /**
+     *
+     * @param str
+     * @return
+     *
+     * example : alok -> lako
+     * alokm  ->  lakom
+     */
+
+    public String getSwappedString(String str) {
+        String result = "";
+        for (int i = 0, j = 1; i < str.length() || j < str.length(); i += 2, j += 2) {
+            if (j < str.length())
+                result = result + str.charAt(j);
+            if (i < str.length())
+                result = result + str.charAt(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String str = "alokmishra";
+        String res = new SwapAlternateAlphabet().getSwappedString(str);
+
+        System.out.println(str + " after swapping alternate char : " + res);
+    }
+}
