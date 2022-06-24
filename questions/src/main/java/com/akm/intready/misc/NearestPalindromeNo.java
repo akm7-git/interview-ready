235fdd9fed74edc84d56210d504f086759ee7d5d
akm7-git <alok.mishra.git@gmail.com>
2022-06-24 12:20:30 +0530

    Palindrome related questions

--- questions/src/main/java/com/akm/intready/misc/NearestPalindromeNo.java
+++ questions/src/main/java/com/akm/intready/misc/NearestPalindromeNo.java
@@ -14,7 +14,34 @@ public class NearestPalindromeNo {
         return rev;
     }
 
+    public int getNearestPalindromeNoWithoutUsingFunction(int n) {
+
+        PalindromeNo pn = new PalindromeNo();
+        for (int i = n; i < 2 * n; i++) {
+            if (pn.isPalindrome(i)) {
+                System.out.println("Found a palindrome no");
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getNearestPalindromeRecursive(int n) {
+
+        PalindromeNo pn = new PalindromeNo();
+        if (pn.isPalindrome(n)) {
+            System.out.println("Nearest palindrome is " + n);
+            return n;
+        }
+        return getNearestPalindromeRecursive(n + 1);
+    }
+
     public static void main(String[] args) {
-        new NearestPalindromeNo().getNearestpalindromeNo(135942);
+        System.out.println("####### Approach-1: solution for nearest palindrome using inbuilt function #####");
+        new NearestPalindromeNo().getNearestpalindromeNo(171);
+        System.out.println("####### Approach-2: solution for nearest palindrome without using inbuilt function #####");
+        new NearestPalindromeNo().getNearestPalindromeNoWithoutUsingFunction(171);
+        System.out.println("####### Approach-3: recursive solution for nearest palindrome #####");
+        new NearestPalindromeNo().getNearestPalindromeRecursive(171);
     }
 }
