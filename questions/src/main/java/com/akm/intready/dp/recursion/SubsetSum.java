73e097a56f8a659dc31d6e26bbb56ddac02553b5
akm7-git <alok.mishra.git@gmail.com>
2022-06-27 21:04:48 +0530

    DP first question

---
+++ questions/src/main/java/com/akm/intready/dp/recursion/SubsetSum.java
@@ -0,0 +1,68 @@
+package com.akm.intready.dp.recursion;
+
+public class SubsetSum {
+
+    public boolean DoesASolutionExist(int[] arr, int endIndex, int sum) {
+        boolean solutionExists = false;
+        if (sum == 0) {
+            return true;
+        }
+        if (endIndex >= 0 && arr[endIndex] <= sum) {
+
+            solutionExists = DoesASolutionExist(arr, endIndex - 1, sum - arr[endIndex])
+                    || DoesASolutionExist(arr, endIndex - 1, sum);
+        } else if (endIndex >= 0 && arr[endIndex] > sum) {
+            solutionExists = solutionExists || DoesASolutionExist(arr, endIndex - 1, sum);
+        }
+        return solutionExists;
+    }
+
+    public boolean DoesASolutionExistWithDpMemoization(int[] arr, int endIndex, int sum) {
+        char matrix[][] = new char[endIndex + 1][sum + 1];
+        for (int i = 0; i < sum + 1; i++) {
+            matrix[0][i] = 'f';
+        }
+        for (int j = 0; j < endIndex + 1; j++) {
+            matrix[j][0] = 't';
+        }
+
+        for (int i = 1; i < sum + 1; i++) {
+            for (int j = 1; j < endIndex + 1; j++) {
+                matrix[j][i] = 'n';
+            }
+        }
+
+        if (matrix[endIndex][sum] == 't') {
+            return true;
+        } else if (matrix[endIndex][sum] == 'f') {
+            return false;
+        }
+        boolean solutionExists = false;
+        if (sum == 0) {
+            return true;
+        }
+        if (endIndex >= 0 && arr[endIndex] <= sum) {
+
+            solutionExists = DoesASolutionExistWithDpMemoization(arr, endIndex - 1, sum - arr[endIndex])
+                    || DoesASolutionExistWithDpMemoization(arr, endIndex - 1, sum);
+        } else if (endIndex >= 0 && arr[endIndex] > sum) {
+            solutionExists = solutionExists || DoesASolutionExistWithDpMemoization(arr, endIndex - 1, sum);
+        }
+        if (solutionExists) {
+            matrix[endIndex][sum] = 't';
+        } else {
+            matrix[endIndex][sum] = 'f';
+        }
+        return solutionExists;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {2, 3, 5, 8, 7};
+        boolean solutionExists = new SubsetSum().DoesASolutionExist(arr, 4, 11);
+        System.out.println("solution exists : " + solutionExists);
+
+        solutionExists = new SubsetSum().DoesASolutionExistWithDpMemoization(arr, 4, 11);
+        System.out.println("solution exists : " + solutionExists);
+    }
+}
